ebb13595a7d87069016ddeb4e43d34a107dc924f
Cấn Thái Sơn <zoubis2001@gmail.com>
2023-09-26 16:17:28 +0700

    connect to database

--- webapp-server/src/app.module.ts
+++ webapp-server/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: ['.env'] }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (process: ConfigService) => ({
+        type: 'postgres',
+        host: process.get('DATABASE_HOST'),
+        port: process.get('DATABASE_PORT'),
+        username: process.get('DATABASE_USERNAME'),
+        password: process.get('DATABASE_PASSWORD'),
+        synchronize: process.get('DATABASE_SYNC'),
+        database: process.get('DATABASE_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      }),
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
